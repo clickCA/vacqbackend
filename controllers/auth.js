11076d584ab169edbb3ff8730634c6d47cd9732b
clickCA <chanakorn.aramsak@gmail.com>
2024-04-04 17:10:41 +0700

    hande logout login

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,11 +19,7 @@ const sendTokenResponse = (user, statusCode, res) => {
   res
     .status(statusCode)
     .cookie("token", token, options)
-    .json({ success: true, 
-      _id:user._id,
-      name: user.name,
-      email:user.email,
-      token });
+    .json({ success: true, token });
 };
 
 // @desc    Register user
@@ -42,7 +38,6 @@ exports.register = async (req, res, next) => {
 
     // const token = user.getSignedJwtToken();
     // res.status(200).json({ success: true, token });
-    console.log(user);
     sendTokenResponse(user, 200, res);
   } catch (err) {
     res.status(400).json({ success: false });
@@ -90,7 +85,12 @@ exports.login = async (req, res, next) => {
     // res.status(200).json({ success: true, token });
     sendTokenResponse(user, 200, res);
   } catch (err) {
-    return res.status(401).json({ success: false, error: "Unauthorized" });
+    return res
+      .status(401)
+      .json({
+        success: false,
+        msg: "Cannot convert email or password to string",
+      });
   }
 };
 
@@ -100,4 +100,19 @@ exports.login = async (req, res, next) => {
 exports.getMe = async (req, res, next) => {
   const user = await User.findById(req.user.id);
   res.status(200).json({ success: true, data: user });
+};
+
+// @desc    Log user out / clear cookie
+// @route   GET /api/v1/auth/logout
+// @access  Private
+exports.logout = async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
 };
