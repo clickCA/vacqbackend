ff35b46219aff648edaf6e1770b7a7d0e6ccdb66
clickCA <chanakorn.aramsak@gmail.com>
2024-04-04 16:53:54 +0700

    add try catch error

--- controllers/auth.js
+++ controllers/auth.js
@@ -54,40 +54,44 @@ exports.register = async (req, res, next) => {
 // @access  Public
 
 exports.login = async (req, res, next) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  // Validate email & password
-  if (!email || !password) {
-    return res.status(400).json({
-      success: false,
-      error: "Please provide an email and password",
-    });
+    // Validate email & password
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        error: "Please provide an email and password",
+      });
+    }
+
+    // Check for user
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ success: false, error: "Invalid credentials" });
+    }
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ success: false, error: "Invalid credentials" });
+    }
+
+    // Create token
+    // const token = user.getSignedJwtToken();
+
+    // res.status(200).json({ success: true, token });
+    sendTokenResponse(user, 200, res);
+  } catch (err) {
+    return res.status(401).json({ success: false, error: "Unauthorized" });
   }
-
-  // Check for user
-
-  const user = await User.findOne({ email }).select("+password");
-
-  if (!user) {
-    return res
-      .status(401)
-      .json({ success: false, error: "Invalid credentials" });
-  }
-
-  // Check if password matches
-  const isMatch = await user.matchPassword(password);
-
-  if (!isMatch) {
-    return res
-      .status(401)
-      .json({ success: false, error: "Invalid credentials" });
-  }
-
-  // Create token
-  // const token = user.getSignedJwtToken();
-
-  // res.status(200).json({ success: true, token });
-  sendTokenResponse(user, 200, res);
 };
 
 // @desc    Get current logged in user
