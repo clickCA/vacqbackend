8858deda1b094769c80cdf4c526d4d3af2e4c62c
clickCA <chanakorn.aramsak@gmail.com>
2024-04-14 12:20:13 +0700

    add security and swagger

--- server.js
+++ server.js
@@ -2,20 +2,63 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const {xss}=require('express-xss-sanitizer');
+const rateLimit = require("express-rate-limit");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express VacQ API",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000/api/v1",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 // Connect to database
 connectDB();
 
 const app = express();
 // Body parser
-var cors = require('cors')
-app.use(cors()) 
 app.use(express.json());
 
+//Sanitize data
+app.use(mongoSanitize());
+
 // Cookie parser
+app.use(cookieParser());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Swagger
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+// Rate limiting
+const limiter = rateLimit({
+  windowsMs: 60000, //10 mins
+  max: 100,
+});
+app.use(limiter);
 
 // Router files
 const hospitals = require("./routes/hospitals");
